59da33ee5ef0a5f8fe3b17d316dc986825c3a4c5
sagar5412 <sagar.gk.bly@gmail.com>
2025-12-01 19:50:20 +0530

    nextauth

---
+++ nextauth/app/lib/auth.tsx
@@ -0,0 +1,10 @@
+import GoogleProvider from "next-auth/providers/google";
+export const NEXT_AUTH = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET || "",
+};
